0ce71338be1c990769260bf13dac7f4da7ac8b9b
jhu03 <jh65814@gmail.com>
2018-07-06 01:50:33 -0700

    added random number of generated balls

--- Week 3/Processing JS/sketch.js
+++ Week 3/Processing JS/sketch.js
@@ -15,19 +15,20 @@ function Circle(x, y, size, color, xSpeed, ySpeed){
     this.xCoor = x;
     this.yCoor = y;
     this.diameter = size;
-    this.color = color; //makes the default color black
+    this.color = color; 
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
 }
 
 var circle = new Circle(0,0,80, [255,0,0], 2, 10);
 var circle2 = new Circle(0, 30, 50, 7, 7);
-var circleList = [circle, circle2, new Circle(100,0,18)];
+/*var circleList = [circle, circle2, new Circle(100,0,18)];*/
 
 
 function setup() {
     createCanvas(parseInt(prompt("Enter canvas width")),parseInt(prompt("Enter canvas height"))); //lets user choose canvas size
     frameRate(50);
+    ballAmount();
 }
 
 
@@ -36,43 +37,35 @@ function randomColor(){
 }
 
 
-//function chooses random number
-function numberSet(set){
-    var random = Math.floor(Math.random() * set.length);
-    return set[random];
+//function chooses random number & loops until circles equals a number within number array
+function ballAmount(){
+    var circles = random([2,5,9,16]);
+    for(var numBall = 0; numBall < circles; numBall ++){
+        circleList.push(new Circle(0,0, 80, [random(0, 255), random(0, 255), random(0, 255)], random(0, 20), random(0, 20))); //adds new circles to empty array
+    };
 }
-
-//set of random numbers to choose from
-var balls = numberSet([2,5,9,16]);
-
 
 //empty array
 var circleList = [];
 
 
-//function that calls for the amount of balls drawn by adding balls into program until it meets the number set amount
-for(var numBall = 0; numBall < balls; numBall ++){
-    var circles = new Circle(0,0, 80, [Math.random()*255, Math.random() * 255, Math.random() *255], Math.random()*20, Math.random()*20); //new circle with random color and speed
-    circleList = circleList.concat([circles]); //adds new circles to empty array
-}
-    
-
 function draw(){
     background(255,255,255); //sets background to white so no color of trails show
 
+    
     for(var i = 0; i < circleList.length; i++){
         fill(circleList[i].color);
         ellipse(circleList[i].xCoor, circleList[i].yCoor, circleList[i].diameter);
         
         //if we hit the right/left side of canvas
         if(circleList[i].xCoor > width || circleList[i].xCoor <0){
-            circleList[i].color = [Math.random()*255, Math.random() * 255, Math.random() *255] //makes the circle random color
+            circleList[i].color = [random(0,255), random(0,255), random(0,255)] //makes the circle random color
             circleList[i].xSpeed = -circleList[i].xSpeed; //reverse direction
 
 
         //if hit top/bottom side of canvas
         }if(circleList[i].yCoor < 0 || circleList[i].yCoor > height){
-            circleList[i].diameter = Math.random()*100; //changes diameter by choosing a random number
+            circleList[i].diameter = random(0,100); //changes diameter by choosing a random number
             circleList[i].ySpeed = -circleList[i].ySpeed; //reverses direction
         }
             
